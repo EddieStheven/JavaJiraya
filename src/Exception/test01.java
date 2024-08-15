dc010f3330d2f7df07de4b8b0ccbd1748aa4d927
EddieStheven <eddiesthevengith@gmail.com>
2024-08-15 12:49:44 -0300

    ATA

---
+++ src/Exception/test01.java
@@ -0,0 +1,24 @@
+package Exception;
+
+import java.io.File;
+import java.io.IOException;
+
+public class test01 {
+
+    public static void main(String[] args) {
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo(){
+        File file = new File("arquivo\\teste.txt");
+        try {
+
+           boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado " +isCriado);
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
